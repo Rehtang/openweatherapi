fea059c0ef48fe3a51fe3cfe7d0de7252ec7e53b
Rehtang <Warrior8584!>
2022-02-28 22:54:37 +0300

    git init

---
+++ src/main/java/ru/rehtang/openweatherapi/OpenweatherapiApplication.java
@@ -0,0 +1,13 @@
+package ru.rehtang.openweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenweatherapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenweatherapiApplication.class, args);
+	}
+
+}
